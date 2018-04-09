205f541c49c635ca4f5299cbf5051a04daee4970
omkar <omkardesai09>
2018-04-09 11:35:05 -0700

    Update in simulation of number of clients code

--- dhcp_cli_sim.py
+++ dhcp_cli_sim.py
@@ -27,11 +27,11 @@ conf.checkIPaddr = False
 # DHCP packets
 # There are 4 types of DHCP packets: Discover, Offer, Request, Acknowledgement
 
-def generate_client_packets():
+all_ip_addr = []
+client_mac_addr = []
+server_ip_addr = []
 
-    all_ip_addr = []
-    client_mac_addr = []
-    server_ip_addr = []
+def generate_client_packets():
 
     x_id = random.randrange(1, 100000)  #x_id = transaction number, random identifier for single DHCP transaction
     cli_mac = '00:15' + str(RandMAC())[5:]
@@ -46,13 +46,14 @@ def generate_client_packets():
     discover_list, offer_list = srp(discover_pkt, iface=dst_inf, timeout=2, verbose=0)
 
     ip_addr_offered = discover_list[0][1][BOOTP].yiaddr
+    print 'ip_addr_offered' + ip_addr_offered
 
     # DHCP request packet
     request_pkt = Ether(src=cli_mac, dst='ff:ff:ff:ff:ff:ff') / IP(src='0.0.0.0', dst='255.255.255.255') / UDP(sport=68, dport=67) / BOOTP(op=1, xid=x_id, chaddr=str_cli_mac) / DHCP(options=[('message-type', 'request'), ('requested_addr', ip_addr_offered), ('end')])
 
     # Sending request packet and receive Ack packet
 
-    request_list, ack_list = srp(request_pkt, iface=dst_inf, timeout=2, verbose=2)
+    request_list, ack_list = srp(request_pkt, iface=dst_inf, timeout=2, verbose=0)
 
     ip_addr_offered_ack = request_list[0][1][BOOTP].yiaddr
 
@@ -78,26 +79,67 @@ try:
     while True:
         #There are three options provided for user to simulate following application
         # 1. Simulate DHCP client, 2. Simulate DHCP release, 3. Exit
-        choice=raw_input("\n Select appropriate option from below: \n1. Simulate DHCP clients\n2. Release DHCP bindings\n3. Exit")
+        choice = raw_input("\n Select appropriate option from below: \n1. Simulate DHCP clients\n2. Release DHCP bindings\n3. Exit\n")
 
-        if choice == 1:
+        if choice == '1':
             cli_no = raw_input('\nEnter number of clients you would like to simulate: ')
             dst_inf = raw_input('\nEnter the interface on which to send packets: ')
 
-            for i in range(0,int(cli_no)):
+            #global ip_leases
+            #global client_mac_addr
+            #global server_ip_addr
+
+            for i in range(0, int(cli_no)):
                 ip_leases = generate_client_packets()[0]
 
             print "\nIP addresses assigned by DHCP server are stored in ip_lease.txt file\n"
 
             ip_addr = open('ip_lease.txt', 'w')
 
-            for index, value in enumerate(ip_leases):
+            for index, value_ip in enumerate(ip_leases):
                 # Below line is a another way to write anything in file OR alternative option for write method
-                print>>ip_addr, value + '\t' + client_mac_addr[index] + '\t' + server_ip_addr[index]
+                print>>ip_addr, value_ip + ',' + client_mac_addr[index] + ',' + server_ip_addr[index]
             ip_addr.close()
 
             continue
 
+        elif choice == '2':
+            next_choice = raw_input('\n1. Release single IP address\n2. Release all IP addresses\n3. Back to previous menu\n')
 
+            if next_choice == '1':
+                user_ip_addr = raw_input('Enter IP address: ')
 
+                if user_ip_addr in ip_leases:
+                    ip_index = ip_leases.index(user_ip_addr)
+                    generate_release_packet(user_ip_addr, client_mac_addr[ip_index], server_ip_addr[ip_index])
+                    print "\nSending release packet...\n"
+
+                else:
+                    print "No IP address found\n"
+                    continue
+
+            elif next_choice == '2':
+                for user_ip_addr in ip_leases:
+                    ip_index = ip_leases.index(user_ip_addr)
+                    generate_release_packet(user_ip_addr, client_mac_addr[ip_index], server_ip_addr[ip_index])
+
+                print "Release packets have been sent"
+
+                ip_release = open('ip_lease.txt', 'w').close()
+                continue
+
+            elif next_choice == '3':
+                break
+
+        elif choice == '3':
+            print "\nCode is exiting.\n"
+            sys.exit()
+
+        else:
+            print "\nChoose correct option\n"
+            continue
+
+except KeyboardInterrupt:
+    print '\nCode is exiting due to keyboard interrupt'
+    sys.exit()
 
